0f38e46f3c6bad220819ba63602e62bee9ba3f87
ahalyajupalli <ahalya.jupalli@gmail.com>
2024-05-22 11:55:47 -0600

    added slideinfo array

--- npm.arc-components/lib/image-carousel/ImageCarousel.vue
+++ npm.arc-components/lib/image-carousel/ImageCarousel.vue
@@ -6,12 +6,30 @@
   >
     <div class="slides-container">
       <div
-        v-for="(slide, index) in urls"
+        v-for="(slide, index) in slideInfo"
         :key="index"
         class="slide"
         :style="{ transform: `translateX(-${currentSlide * 100}%)` }"
       >
-        <img :src="slide" :alt="'Slide ' + (index + 1)" />
+        <div class="slide-content">
+          <img
+            :src="slide.imageUrl"
+            :alt="'Slide ' + (index + 1)"
+            class="slide-image"
+          />
+          <div
+            v-if="slide.title || slide.description || slide.link"
+            class="slide-info"
+          >
+            <h1 v-if="slide.title">{{ slide.title }}</h1>
+            <h4 v-if="slide.description" class="desc-in-carousel">
+              {{ slide.description }}
+            </h4>
+            <a v-if="slide.link" :href="slide.link" class="slide-link">{{
+              slide.linkText || "Learn more"
+            }}</a>
+          </div>
+        </div>
       </div>
     </div>
 
@@ -24,7 +42,7 @@
     <!-- Pagination -->
     <div v-if="displayBottomIndicator" class="pagination">
       <span
-        v-for="(_slide, index) in urls"
+        v-for="(_slide, index) in slideInfo"
         :key="index"
         @click="goToSlide(index)"
         :class="{ active: index === currentSlide }"
@@ -35,9 +53,10 @@
 
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
+import { SlideInfo } from "./models";
 
 const props = defineProps<{
-  urls: string[];
+  slideInfo: SlideInfo[];
   cssClass: string;
   displaySideIndicators?: boolean; // Optional prop
   displayBottomIndicator?: boolean; // Optional prop
@@ -51,7 +70,7 @@ const displaySideIndicators = props.displaySideIndicators ?? true;
 const displayBottomIndicator = props.displayBottomIndicator ?? true;
 
 const nextSlide = () => {
-  if (currentSlide.value < props.urls.length - 1) {
+  if (currentSlide.value < props.slideInfo.length - 1) {
     currentSlide.value++;
   } else {
     currentSlide.value = 0;
@@ -62,7 +81,7 @@ const prevSlide = () => {
   if (currentSlide.value > 0) {
     currentSlide.value--;
   } else {
-    currentSlide.value = props.urls.length - 1;
+    currentSlide.value = props.slideInfo.length - 1;
   }
 };
 
@@ -73,7 +92,7 @@ const goToSlide = (index: number) => {
 const startAutoScroll = () => {
   autoScrollInterval.value = setInterval(() => {
     nextSlide();
-  }, 2000);
+  }, 3000);
 };
 
 const stopAutoScroll = () => {
@@ -89,4 +108,73 @@ onMounted(() => {
 
 <style scoped>
 @import "./styles.module.css";
+
+.slide {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  transition: transform 0.5s ease-in-out;
+}
+
+.slide-content {
+  display: flex;
+  width: 100%;
+}
+
+.slide-image {
+  flex: 1;
+  max-width: 50%;
+  object-fit: cover;
+}
+
+.slide-info {
+  flex: 1;
+  padding: 20px;
+}
+
+.slide-info h1 {
+  margin-top: 0;
+}
+
+.slide-info h4.desc-in-carousel {
+  font-size: 1.25rem;
+  margin-top: 0.5rem;
+}
+.desc-in-carousel {
+  font-size: 1.25rem;
+  margin-top: 0.5rem;
+}
+.slide-link {
+  color: #00f;
+  text-decoration: underline;
+  display: block;
+  margin-top: 10px;
+}
+
+.navigation .prev-btn,
+.navigation .next-btn {
+  background: transparent;
+  border: none;
+  font-size: 2rem;
+  cursor: pointer;
+}
+
+.pagination {
+  display: flex;
+  justify-content: center;
+  margin-top: 10px;
+}
+
+.pagination span {
+  display: inline-block;
+  width: 30px;
+  height: 3px;
+  background: #ccc;
+  margin: 0 5px;
+  cursor: pointer;
+}
+
+.pagination span.active {
+  background: #333;
+}
 </style>
--- npm.arc-components/src/App.vue
+++ npm.arc-components/src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
     <ImageCarousel
-      :urls="imageList1"
+      :slideInfo="slideInfo1"
       cssClass="carousel-one"
       :displaySideIndicators="true"
       :displayBottomIndicator="true"
     />
     <ImageCarousel
-      :urls="imageList2"
+      :slideInfo="slideInfo2"
       cssClass="carousel-two"
       :displaySideIndicators="true"
       :displayBottomIndicator="false"
     />
     <ImageCarousel
-      :urls="imageList3"
+      :slideInfo="slideInfo3"
       cssClass="carousel-three"
       :displaySideIndicators="false"
       :displayBottomIndicator="true"
@@ -23,6 +23,7 @@
 
 <script setup lang="ts">
 import { ImageCarousel } from "../lib/main";
+import { SlideInfo } from "../lib/image-carousel/models";
 import image1 from "../assets/image-carousel/image_01.jpg";
 import image2 from "../assets/image-carousel/image_02.jpg";
 import image3 from "../assets/image-carousel/image_03.jpg";
@@ -33,11 +34,52 @@ import image7 from "../assets/image-carousel/image_07.jpg";
 import image8 from "../assets/image-carousel/image_08.jpg";
 import image9 from "../assets/image-carousel/image_09.jpg";
 
-const imageList1 = [image1, image2, image3];
-const imageList2 = [image4, image5, image6];
-const imageList3 = [image7, image8, image9];
+const slideInfo1: SlideInfo[] = [
+  {
+    imageUrl: image1,
+    title: "Humming Bird",
+    description:
+      "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book.",
+    link: "https://pixabay.com/photos/bird-hummingbird-ornithology-fly-8733716/",
+    linkText: "Read more",
+  },
+  {
+    imageUrl: image2,
+    title: "Black Bird",
+    description:
+      "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book.",
+  },
+  { imageUrl: image3 },
+];
+
+const slideInfo2: SlideInfo[] = [
+  {
+    imageUrl: image4,
+    title: "Red bird",
+    description:
+      "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book.",
+    link: "https://pixabay.com/photos/bird-branch-perched-feathers-2295431/",
+    linkText: "Read more",
+  },
+  { imageUrl: image5, title: "Green Bird" },
+  { imageUrl: image6 },
+];
+
+const slideInfo3: SlideInfo[] = [
+  {
+    imageUrl: image7,
+    description:
+      "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book.",
+  },
+  { imageUrl: image8, title: "Title 8", link: "#", linkText: "Read more" },
+  { imageUrl: image9 },
+];
 </script>
 
 <style>
-@import "../lib/image-carousel/styles.module.css";
+#app {
+  display: flex;
+  flex-direction: column;
+  gap: 20px; /* Adds space between each carousel */
+}
 </style>
